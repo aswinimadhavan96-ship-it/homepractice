0b2371c7c90dab83a6baa73cdd7a7e3d1073be42
aswinimadhavan96-ship-it <aswinimadhavan96@gmail.com>
2025-12-23 14:04:14 +0530

    Add files via upload

---
+++ Q10si.java
@@ -0,0 +1,19 @@
+class Q10si
+{
+	public static void main(String[] args)
+	{
+		int p=10000;
+		double r=0.05f;
+		int t=3;
+		double si=(p*r*t);
+		System.out.println("SIMPLE INTEREST=" +si);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
