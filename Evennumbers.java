14eb15fc2ca38428a5bb3bbdecffeaddac30351a
aswinimadhavan96-ship-it <aswinimadhavan96@gmail.com>
2025-12-30 23:20:04 +0530

    loop-day12

---
+++ Evennumbers.java
@@ -0,0 +1,17 @@
+class Evennumbers
+{
+	public static void main(String[] args)
+	{
+		int a=2;
+		System.out.println("Even numbers 1 to 30");
+		
+	while(a<=30)
+	{
+		{
+			if(a%2==0)
+			System.out.println(a+"is a even number");
+			a++;
+		}
+	}
+	}
+}
---
+++ Evento30.java
@@ -0,0 +1,17 @@
+class Evento30
+{
+	public static void main(String[] args)
+	{
+		int a=2;
+		System.out.println("Even numbers 1 to 30");
+		
+	do
+	{
+		{
+			if(a%2==0)
+			System.out.println(a+"is a even number");
+			a++;
+		}
+	}while(a<=30);
+	}
+}
