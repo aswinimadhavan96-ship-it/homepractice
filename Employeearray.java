ed107bc0eb91266ba20d0905184187b970a2b6c6
aswinimadhavan96-ship-it <aswinimadhavan96@gmail.com>
2026-01-06 16:25:10 +0530

    ARRAY-HW

---
+++ Employeearray.java
@@ -0,0 +1,11 @@
+class Employeearray
+{
+	public static void main(String[] args)
+	{
+		String ar[]={"Software Engineer","Data Analyst","Project Manager","HR Manager","UI/UX Designer"};
+		for(int i=0;i<ar.length;i++)
+		{
+			System.out.println("Employee:"+ar[i]);
+		}
+		}
+}
