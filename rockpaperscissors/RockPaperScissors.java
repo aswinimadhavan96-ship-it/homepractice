ba0ec678bd26038f8829d82c6f6a8f424df28bd6
aswinimadhavan96-ship-it <aswinimadhavan96@gmail.com>
2026-03-09 12:49:43 +0530

    B-homework

---
+++ rockpaperscissors/RockPaperScissors.java
@@ -0,0 +1,38 @@
+package rockpaperscissors;
+import java.util.Random;
+import java.util.Scanner;
+public class RockPaperScissors {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        System.out.println("Rock Paper Scissors Game");
+        System.out.println("1 -> Rock");
+        System.out.println("2 -> Paper");
+        System.out.println("3 -> Scissors");
+
+        System.out.print("Enter your choice: ");
+        int userChoice = sc.nextInt();
+
+        int computerChoice = rand.nextInt(3) + 1;
+
+        System.out.println("Computer choice: " + computerChoice);
+
+        if(userChoice == computerChoice) {
+            System.out.println("It's Draw!");
+        }
+        else if((userChoice == 1 && computerChoice == 3) ||
+                (userChoice == 2 && computerChoice == 1) ||
+                (userChoice == 3 && computerChoice == 2)) {
+            System.out.println("Your the Winner!!!!!!");
+        }
+        else {
+            System.out.println("Computer Wins.....");
+        }
+
+        sc.close();
+  
+    }
+    
+}
